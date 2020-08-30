f8052a56b2531d1825eb6620afa831389bc074f1
Ilgsson Braga <ilgssonbraga@gmail.com>
2020-08-30 13:54:19 -0300

    feat: Delete and Update Todos

--- src/controllers/todo.ts
+++ src/controllers/todo.ts
@@ -35,7 +35,70 @@ export default {
         };
       },
 
-    getTodoById: () => {},
-    updateTodoById: async () => {},
-    deleteTodoById: () => {},
-  };
+      getTodoById: (
+        { params, response }: { params: { id: string }; response: any },
+      ) => {
+        const todo: Todo | undefined = todos.find((t) => t.id === params.id);
+        if (!todo) {
+          response.status = 404;
+          response.body = {
+            success: false,
+            message: "No todo found",
+          };
+          return;
+        }
+    
+        // If todo is found
+        response.status = 200;
+        response.body = {
+          success: true,
+          data: todo,
+        };
+      },
+      updateTodoById: async (
+        { params, request, response }: {
+          params: { id: string },
+          request: any,
+          response: any,
+        },
+      ) => {
+        const todo: Todo | undefined = todos.find((t) => t.id === params.id);
+        if (!todo) {
+          response.status = 404;
+          response.body = {
+            success: false,
+            message: "No todo found",
+          };
+          return;
+        }
+    
+        // if todo found then update todo
+        const body = await request.body().value;
+        const updatedData: { todo?: string; isCompleted?: boolean } = body;
+        let newTodos = todos.map((t) => {
+          return t.id === params.id ? { ...t, ...updatedData } : t;
+        });
+        response.status = 200;
+        response.body = {
+          success: true,
+          data: newTodos,
+        };
+      },
+      /**
+       * @description Delete todo by id
+       * @route DELETE todos/:id
+       */
+      deleteTodoById: (
+        { params, response }: { params: { id: string }; response: any },
+      ) => {
+        const allTodos = todos.filter((t) => t.id !== params.id);
+    
+        // remove the todo w.r.t id & return
+        // remaining todos
+        response.status = 200;
+        response.body = {
+          success: true,
+          data: allTodos,
+        };
+      },
+    };
